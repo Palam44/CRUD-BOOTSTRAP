640d1b5c7ad9134bb1563a20c212decf8e7abc76
Palam44 <dpalam44@gmail.com>
2022-09-08 23:18:20 +0300

    2

--- src/main/java/ru/kata/spring/boot_security/util/DefaultUser.java
+++ src/main/java/ru/kata/spring/boot_security/util/DefaultUser.java
@@ -1,55 +1,55 @@
-package ru.kata.spring.boot_security.util;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.stereotype.Component;
-import ru.kata.spring.boot_security.model.Role;
-import ru.kata.spring.boot_security.model.User;
-import ru.kata.spring.boot_security.service.RoleService;
-import ru.kata.spring.boot_security.service.UserService;
-
-@Component
-public class DefaultUser {
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private RoleService roleService;
-
-    @PostConstruct
-    private void initialize() {
-        Role roleAdmin = new Role("ROLE_ADMIN");
-        Role roleUser = new Role("ROLE_USER");
-        roleService.saveRole(roleAdmin);
-        roleService.saveRole(roleUser);
-
-
-        User admin = new User();
-        admin.setAge(23);
-        admin.setFistName("Anton");
-        admin.setLastName("Teplov");
-        admin.setEmail("1@1");
-        admin.setUsername("Anna");
-        admin.setPassword("123");
-
-        admin.addRole(roleAdmin);
-
-
-        User user = new User();
-        user.setFistName("user");
-        user.setLastName("user");
-        user.setEmail("12@12");
-        user.setAge(1);
-        user.setUsername("user");
-        user.setPassword("1");
-        user.addRole(roleUser);
-
-        userService.add(admin);
-        userService.add(user);
-
-
-    }
-}
+//package ru.kata.spring.boot_security.util;
+//
+//import javax.annotation.PostConstruct;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import org.springframework.stereotype.Component;
+//import ru.kata.spring.boot_security.model.Role;
+//import ru.kata.spring.boot_security.model.User;
+//import ru.kata.spring.boot_security.service.RoleService;
+//import ru.kata.spring.boot_security.service.UserService;
+//
+//@Component
+//public class DefaultUser {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private RoleService roleService;
+//
+//    @PostConstruct
+//    private void initialize() {
+//        Role roleAdmin = new Role("ROLE_ADMIN");
+//        Role roleUser = new Role("ROLE_USER");
+//        roleService.saveRole(roleAdmin);
+//        roleService.saveRole(roleUser);
+//
+//
+//        User admin = new User();
+//        admin.setAge(23);
+//        admin.setFistName("Anna");
+//        admin.setLastName("Teplova");
+//        admin.setEmail("1@1");
+//        admin.setUsername("Anna");
+//        admin.setPassword("123");
+//
+//        admin.addRole(roleAdmin);
+//
+//
+//        User user = new User();
+//        user.setFistName("user");
+//        user.setLastName("user");
+//        user.setEmail("12@12");
+//        user.setAge(1);
+//        user.setUsername("user");
+//        user.setPassword("1");
+//        user.addRole(roleUser);
+//
+//        userService.add(admin);
+//        userService.add(user);
+//
+//
+//    }
+//}
